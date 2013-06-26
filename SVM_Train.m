de052069e237320dfd6363189a60de7d5ae42dc2
safeng <sh.fung.hku@gmail.com>
2013-06-26 18:12:45 +0800

    Begin adding linear svm training programs

---
+++ SVM_Train.m
@@ -0,0 +1,13 @@
+function SVMStruct = SVM_Train(Training,Group)
+% SVM_TRAIN train severl linear SVMs based on the third dimension of
+% training.
+% SVMStruct = SVM_Train(Training,Group) receives training data and
+% corresponding labels. Returns SVMStruct struct vectors for each landmark
+%
+% SEE ALSO:
+% PrepareTrainingData.m
+
+%% Check dimensionality of training data
+
+%% Traing linear svms for each landmark
+
