3d86e8ff183729ddf80091def50d967899b2750e
safeng <sh.fung.hku@gmail.com>
2013-06-26 17:30:31 +0800

    I finished PrepareTrainingData.m and added short description in README.md

--- PrepareTrainingData.m
+++ PrepareTrainingData.m
@@ -7,11 +7,10 @@ function [Training, Group] = PrepareTrainingData(fileList,landmark,param)
 % labels into Group, which can be used for further training
 %
 % Output:
-% Training - M-by-N-by-K matrix. M corresponds to M observations. N is
-% number of dimensions. K is the group number of training data.
+% Training - 5M-by-N-by-K matrix. M corresponds to M positive observations. 
+% N is number of dimensions. K is the group number of training data.
 %
-% Group - M-by-K logical matrix. Each row corresponds to group label. K is
-% the group number of training data
+% Group - 5M-by-1 logical matrix. Each row corresponds to group label
 %
 % Input:
 % fileList - cell array of image file names
@@ -48,10 +47,13 @@ end
 K = K2/2; % number of landmarks per face (number of classifiers)
 N = prod(stdPatchSize); % feature dimension
 Training = zeros(M*5, N, K); % 1 positive samples + 4 negative samples
-Group = true(M,K);
+% samples arranged in true, false, false, false, false order (1 pos sample + 
+% 4 neg sample neighbors)
+Group = false(M*5,1);
+Group(1:5:end) = true; % mark as true
 
 %% Extract landmarks from image
-for idx = 1:M
+for idx = 1:M % idxth image
     image = imread(fileList{idx});
     % color image
     if ndims(image)==3
@@ -71,7 +73,8 @@ for idx = 1:M
     %% Extract features indexed by feature locations
     featLoc = landmark(idx,:); % corresponding K feature locations
     featLoc = reshape(featLoc,K,2); % get K-by-2 matrix
-    for iMark = 1:K
+    sampleIdx = 1+(idx-1)*5;
+    for iMark = 1:K % iMarkth feature
         % extract image patch centered at each landmark
         feat2D = featLoc(iMark,:);
         patchHalfSize = floor(defaultPatchSize./2);
@@ -82,21 +85,35 @@ for idx = 1:M
         % resize to standard size
         featData = imresize(featData, stdPatchSize);
         % store positive sample in training data matrix
-        Training(idx,:,iMark) = featData(:);
+        Training(sampleIdx,:,iMark) = featData(:); % pos
         %% Extract negative samples
         % top left 
         topLeft = feat2D - defaultPatchSize;
         bottomRight = feat2D;
+        featData = image(topLeft(1):bottomRight(1),topLeft(2):bottomRight(2));
+        featData = imresize(featData, stdPatchSize);
+        Training(sampleIdx+1,:,iMark) = featData(:); % top left neg sample
+   
         % top right
         topLeft = [feat2D(1),feat2D(2)-defaultPatchSize(2)];
         bottomRight = [feat2D(1)+defaultPatchSize(1),feat2D(2)];
+        featData = image(topLeft(1):bottomRight(1),topLeft(2):bottomRight(2));
+        featData = imresize(featData, stdPatchSize);
+        Training(sampleIdx+2,:,iMark) = featData(:); % top right neg sample
+        
         % bottom left
         topLeft = [feat2D(1)-defaultPatchSize(1),feat2D(2)];
         bottomRight = [feat2D(1),feat2D(2)+defaultPatchSize(2)];
+        featData = image(topLeft(1):bottomRight(1),topLeft(2):bottomRight(2));
+        featData = imresize(featData, stdPatchSize);
+        Training(sampleIdx+3,:,iMark) = featData(:); % bottom left neg sample
+        
         % bottom right
         topLeft = feat2D;
         bottomRight = feat2D+defaultPatchSize;
+        featData = image(topLeft(1):bottomRight(1),topLeft(2):bottomRight(2));
+        featData = imresize(featData, stdPatchSize);
+        Training(sampleIdx+4,:,iMark) = featData(:); % bottom right neg sample
+        
     end % for iMark of landmark
 end % for idx of image
-
-%% 
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ ILC_LandmarkDetector
 ====================
 
 landmark detector training (MATLAB)
+This project is used to train landmark detectors using linear SVM and logistic regression.
+
+Given training samples and landmark annotations, features are extracted in PrepareTrainingData.m. Linear SVMs are trained in SVM_Train.m. Finally, logistic.m performs logistic regression on results of linear SVMs.
+
+Author @ Feng Shuang
